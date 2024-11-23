78547aed76dc009f58377e5b057a5195c0a2c433
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-11-23 21:10:10 +0200

    aaasss

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -3,7 +3,9 @@ import { POST_QUERY } from '@/sanity/lib/queries'
 import { Post } from '@/components/Post'
 import { notFound } from 'next/navigation'
 
-type PostIndexProps = { params: { slug: string } }
+type PostIndexProps = {
+  params: Promise<{ slug: string }>
+}
 
 const options = { next: { revalidate: 60 } }
 
@@ -20,3 +22,5 @@ export default async function Page({ params }: PostIndexProps) {
     </main>
   )
 }
+
+
