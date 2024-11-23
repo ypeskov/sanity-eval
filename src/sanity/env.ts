3cf05435b973cbf4e48d60094a3599b4b14223e2
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-11-23 18:52:37 +0200

    qqq

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -5,6 +5,7 @@ export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 );
+console.log('projectId', projectId);
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
